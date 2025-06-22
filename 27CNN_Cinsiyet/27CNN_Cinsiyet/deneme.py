e2ba988fcfaeb28ecbf83568fbe065eb57f52053
jesusamaisa <muhammed224098@ogr.duzce.edu.tr>
2025-06-22 18:27:20 +0300

    ML

---
+++ 27CNN_Cinsiyet/27CNN_Cinsiyet/deneme.py
@@ -0,0 +1,3 @@
+l = [0.1,0.2,0.3,0.9,0.4]
+l=list(map(round,l))
+print(l)
---
+++ ilk.py
@@ -0,0 +1,16 @@
+#Ders 7
+#Verilerin içeri aktarılması
+#kütüphaneler
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Kod Bölümü
+#veri yükleme
+
+veriler = pd.read_csv('veriler.csv')
+#pd.read_csv("veriler.csv") ayniiiisi
+
+
+
+#veri ön işleme
